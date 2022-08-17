e4fa1a4289b6a0521747e72ce76ce60d80ac0c8c
Mazz33 <HamMaz@protonmail.com>
2022-08-17 17:17:50 +0400

    moved import to notes.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import "pages.dart";
-import "notes.dart";
 
 void main() {
   runApp(const Noted());
--- lib/pages.dart
+++ lib/pages.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "notes.dart";
 
 RoutesManager router = RoutesManager();
 
